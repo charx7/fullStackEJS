d14fb437e05c0b3f759694e7bd824a0363748f99
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-12-02 01:50:33 -0600

    Pasando argumentos de las urls a los HTMLS rendereados

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ var app = express();
 app.get("/", function(request, response) {
     // Para que la respuesta envie una pagina de HTML separada (embeeded javascript)
     response.render("home.ejs");
-    
+
 });
 
+// Otra ruta para sacar elementos desplegados dependiendo de routing
+app.get("/enamorarse/:puerquito", function(request, response){
+    // Salva lo de la ruta en una variable
+    var despliegue = request.params.puerquito;
+    // Renderear el archivo
+    response.render("love.ejs", {desplegar: despliegue});
+});
 
 
 // Listener del puerto para iniciar 
